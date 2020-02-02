46932acf89bdb7673c6c5737fedb966ec41cf167
wongalv <alvinkwong1@gmail.com>
2020-02-02 10:42:39 -0500

    update

---
+++ javascript timer project/timer.js
@@ -0,0 +1,11 @@
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const restartButton = document.getElementById("restart");
+
+let hoursText = document.getElementById("hours");
+let minutesText = document.getElementById("minutes");
+let secondsText = document.getElementById("seconds");
+
+let hours
+let minutes
+let seconds
